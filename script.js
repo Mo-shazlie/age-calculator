3b07e6ce50a8d64e307bed7610cbd02e83d83177
Mo-shazlie <mouroto26@gmail.com>
2026-07-31 05:16:49 +0300

    initial

---
+++ script.js
@@ -0,0 +1,18 @@
+let birthYear = document.getElementById("BirthYear");
+let button = document.getElementById("Button");
+let result = document.getElementById("result");
+
+button.addEventListener("click", function () {
+  let theYear = birthYear.value;
+  if (theYear === "") {
+    result.textContent = "Please enter your birth year";
+    return;
+  }
+  if (theYear > 2026) {
+    result.textContent = "don't play with me, you are't born yet??";
+    return;
+  }
+
+  let age = 2026 - theYear;
+  result.textContent = `Your age is ${age}`;
+});
